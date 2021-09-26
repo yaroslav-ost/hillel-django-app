9b27deff72ce93895dc3c89e14ff05b3e63be88b
Yaroslav Mokliak <yaroslav.mokliak@nure.ua>
2021-09-26 17:00:36 +0300

    Initialize

---
+++ settings.py
@@ -0,0 +1,25 @@
+import os
+DEBUG=True,
+ROOT_URLCONF='zen'
+
+SECRET_KEY = 'Test'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth'
+]
