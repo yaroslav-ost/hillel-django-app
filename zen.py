b3c92dc037d334b3ffb104e5ae405f1745e156e6
Yaroslav Mokliak <yaroslav.mokliak@nure.ua>
2021-09-26 17:25:57 +0300

    Formatting

--- zen.py
+++ zen.py
@@ -1,24 +1,19 @@
 import importlib
-
+import this
 from django.core.management import execute_from_command_line
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path, re_path
-from django.views.generic.base import RedirectView
-import this
 from random import choice
-
 
 text = ''.join(this.d.get(c, c) for c in this.s)
 title, _, *quotes = text.splitlines()
+
 
 def get_favicon(request):
     image_data = open("favicon.png", "rb").read()
     return HttpResponse(image_data)
 
-def get_homepage(request):
-    context = {'title': title, 'message':choice(quotes)}
-    return render(request,'homepage.html',context)
 
 def get_module(mod_name):
     try:
@@ -27,31 +22,35 @@ def get_module(mod_name):
         return
     return module
 
-def get_attribute(module,att_name):
+
+def get_attribute(module, att_name):
     try:
-        attribute = getattr(module,att_name)
+        attribute = getattr(module, att_name)
     except AttributeError:
         return
     return attribute
 
-def get_mod_page(request,mod_name):
-    module = get_module(mod_name)
-    if module is None:
-       return HttpResponse(f"No module named '{mod_name}'", status=404)
-    attributes = [func for func in dir(module) if not func.startswith('_')]
-    context={'title':mod_name,'mod': mod_name, 'attributes':attributes}
-    return render(request,'modpage.html',context)
+def get_homepage(request):
+    context = {'title': title, 'message': choice(quotes)}
+    return render(request, 'homepage.html', context)
 
-
-def get_att_page(request,mod_name,att_name):
+def get_mod_page(request, mod_name):
     module = get_module(mod_name)
     if module is None:
         return HttpResponse(f"No module named '{mod_name}'", status=404)
-    attribute=get_attribute(module,att_name)
+    attributes = [func for func in dir(module) if not func.startswith('_')]
+    context = {'title': mod_name, 'mod': mod_name, 'attributes': attributes}
+    return render(request, 'modpage.html', context)
+
+
+def get_att_page(request, mod_name, att_name):
+    module = get_module(mod_name)
+    if module is None:
+        return HttpResponse(f"No module named '{mod_name}'", status=404)
+    attribute = get_attribute(module, att_name)
     if attribute is None:
         return HttpResponse(f"Module '{mod_name}' has no attribute '{att_name}'", status=404)
-    return HttpResponse(attribute.__doc__,content_type='text/plain')
-
+    return HttpResponse(attribute.__doc__, content_type='text/plain')
 
 
 urlpatterns = [
@@ -60,7 +59,6 @@ urlpatterns = [
     path('doc/<mod_name>', get_mod_page),
     path('doc/<mod_name>/<att_name>', get_att_page),
 ]
-
 
 if __name__ == '__main__':
     execute_from_command_line()
